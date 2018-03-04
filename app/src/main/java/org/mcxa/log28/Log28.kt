163a1f74b6b96dde8cc723ff433ac1bf84615065
Zoe Anderson <>
2018-03-04 14:19:12 -0500

    Moved crash reporting to http

--- app/src/main/java/org/mcxa/log28/Log28.kt
+++ app/src/main/java/org/mcxa/log28/Log28.kt
@@ -6,11 +6,14 @@ import io.realm.Realm
 import org.acra.annotation.*
 import org.acra.ACRA
 import org.acra.annotation.AcraCore
+import org.acra.sender.HttpSender
 
 
-
-//@AcraCore(buildConfigClass = BuildConfig::class)
-//@AcraMailSender(mailTo = "crashreport@log28.com")
+@AcraCore(buildConfigClass = BuildConfig::class)
+@AcraHttpSender(uri = "http://crash.log28.com:55000/send", httpMethod = HttpSender.Method.POST)
+@AcraNotification(resTitle = R.string.crash_title,
+        resText = R.string.crash_text,
+        resChannelName = R.string.notification_channel)
 class Log28 : Application() {
 
     override fun onCreate() {
@@ -18,9 +21,8 @@ class Log28 : Application() {
         Realm.init(this)
     }
 
-    /*override fun attachBaseContext(base: Context?) {
+    override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-
         ACRA.init(this);
-    }*/
+    }
 }
