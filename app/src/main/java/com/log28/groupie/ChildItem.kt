03b3d361e7a058f068cd536036293c5de445610b
Zoe Anderson <>
2018-04-07 11:37:58 -0400

    Fixed tiny error where checkboxes didn't persist there state

--- app/src/main/java/com/log28/groupie/ChildItem.kt
+++ app/src/main/java/com/log28/groupie/ChildItem.kt
@@ -14,6 +14,7 @@ class ChildItem(val symptom: Symptom, var state: Boolean, var onClick: () -> Uni
 
         viewHolder.list_item.setOnClickListener {
             viewHolder.item_checkbox.toggle()
+            state = !state
             onClick.invoke()
         }
     }
